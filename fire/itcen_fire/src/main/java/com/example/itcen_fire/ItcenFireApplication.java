7f23a66ea809aa7f38bc5c1a484934b63251d1ac
choi <choi@naver.com>
2025-05-07 16:47:28 +0900

    Initial

---
+++ fire/itcen_fire/src/main/java/com/example/itcen_fire/ItcenFireApplication.java
@@ -0,0 +1,13 @@
+package com.example.itcen_fire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcenFireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItcenFireApplication.class, args);
+	}
+
+}
---
+++ fire/itcen_fire/src/test/java/com/example/itcen_fire/ItcenFireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itcen_fire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcenFireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
